0e6d233219aa8c773fcdbf28ccf67d9183d097a2
Himanshu Sagar <hsagarthegr8@gmail.com>
2018-08-25 03:15:52 +0530

    Implemented Data Access Layer Code

---
+++ server/server.js
@@ -0,0 +1,51 @@
+const express = require('express')
+const app = express()
+
+const Dao = require('./modules/data-access/data-access')
+
+const dao = new Dao()
+
+app.get('/rest/api/users/get/', async (req, res) => {
+    let result = await dao.find("users")
+    res.send(result)
+})
+
+app.post('/rest/api/users/add', async (req, res) => {
+    obj = {
+        userName: 'soumya145',
+        name: 'soumya',
+        email: 'soumyaN@hotmail.com',
+        mobile: '9123876903',
+        gender: 'F', dateOfBirth:
+            new Date("1996-07-01"),
+        isVerified: false,
+        isDeleted: false
+    }
+    let result
+    try {
+        result = await dao.insert("users")
+    }
+    catch {
+        result = {error:"err"}
+    }
+    res.send(result)
+})
+
+app.delete('/rest/api/users/delete/', async (req, res) => {
+    let result = await dao.delete("users", { name: 'soumya' })
+    res.send(result)
+})
+
+app.patch('/rest/api/users/update/', async (req, res) => {
+    let result
+    try {
+        result = await dao.update("users",{ name: 'soumya' },{$set:{name:"Soumya"}})
+    } 
+    catch (err) {
+        result = {err:err}
+    }
+    res.send(result)
+})
+
+
+app.listen('8080', () => console.log('Listening on port 8080'))
